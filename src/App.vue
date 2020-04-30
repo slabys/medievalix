a2ba96412fb519fe42068c4d4187b75c7f9b9bdc
BuildTools <unconfigured@null.spigotmc.org>
2020-04-30 13:29:02 +0200

    Medievalix

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <Header msg="Medievalix" />
+    <EventSection />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Header from './components/Header.vue'
+import EventSection from './components/EventSection.vue'
 
 export default {
+
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    EventSection
   }
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+<style lang="scss">
+
+  .wrapper {
+  font-family: Sigmar One, Merriweather, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 0px;
+  margin: 0px;
 }
+
 </style>
---
+++ src/components/EventSection.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="img">
+        <img class="img-fluid" width="100%" src="../assets/images/bg.png">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'EventSection',
+    }
+</script>
+
+<style scoped>
+
+    img{
+        object-fit: cover;
+        max-height: 720px;
+    }
+
+</style>
